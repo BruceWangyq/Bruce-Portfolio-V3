2c9cf78e58587e9008a89e571776038ef59dfeda
brucewang <brucewangyq@gmail.com>
2022-10-12 23:52:16 -0400

    feat: add style engine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,24 @@
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
 
-import "styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import 'styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { StyledEngineProvider } from '@mui/material';
 
 const { chains, provider } = configureChains(
   [chain.goerli],
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
     publicProvider(),
-  ]
+  ],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "Bruce Wang APP",
+  appName: 'Bruce Wang APP',
   chains,
 });
 
@@ -29,13 +30,15 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <StyledEngineProvider injectFirst>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <ThemeProvider attribute="class" disableTransitionOnChange>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </StyledEngineProvider>
   );
 }
 
